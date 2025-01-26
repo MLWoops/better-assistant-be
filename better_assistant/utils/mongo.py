551e75c0cca12d3499146769872591b3d23ca692
siha <zksi67@gmail.com>
2025-01-27 01:20:50 +0900

    UPDATE refactor mongo_client methods for consistency and clarity

--- better_assistant/utils/mongo.py
+++ better_assistant/utils/mongo.py
@@ -2,13 +2,14 @@ import os
 
 from pymongo import MongoClient
 
-from better_asisstant.exceptions import (
+from better_assistant.exceptions import (
     CollectionNotDefinedException,
     DataNotCreatedException,
     DataNotDeletedException,
     DataNotReadException,
     DataNotUpdatedException,
-    NoDataException,
+    DataNotFoundException,
+    NoFilterException,
 )
 
 
@@ -21,12 +22,12 @@ class mongo_client:
         self.client = MongoClient(host=mongo_host, port=int(mongo_port))
         self.db = self.client.get_database(mongo_db)
 
-    def create(self, data, collection=None):
+    def insert(self, data, collection=None):
 
         if not collection:
             raise CollectionNotDefinedException("Collection is required")
         if not data:
-            raise NoDataException("Data is required")
+            raise NoFilterException("Data is required")
         collection = self.db.get_collection(collection)
         result = collection.insert_one(data)
 
@@ -36,51 +37,67 @@ class mongo_client:
             raise DataNotCreatedException("Data not created")
 
 
-    def read(self, data, collection=None):
+    def find(self, filter, collection=None):
 
         if not collection:
             raise CollectionNotDefinedException("Collection is required")
-        if not data:
-            raise NoDataException("Data is required")
+        if not filter:
+            raise NoFilterException("Data is required")
         collection = self.db.get_collection(collection)
-        result = collection.find_one(data)
+        result = collection.find_one(filter)
+
+        if result:
+            return result
+        else:
+            raise DataNotReadException("Data not found")
+        
+    def find_all(self, filter, collection=None):
+        
+        if not collection:
+            raise CollectionNotDefinedException("Collection is required")
+        
+        if not filter:
+            raise NoFilterException("Data is required")
+        
+        collection = self.db.get_collection(collection)
+        result = collection.find(filter)
 
         if result:
             return result
         else:
             raise DataNotReadException("Data not found")
 
-    def update(self, data, new_data, collection=None):
+    def update(self, filter, update, collection=None):
 
         if not collection:
             raise CollectionNotDefinedException("Collection is required")
-        if not data:
-            raise NoDataException("Data is required")
-        if not new_data:
-            raise NoDataException("New data is required")
+        if not filter:
+            raise NoFilterException("Data is required")
+        if not update:
+            raise NoFilterException("New data is required")
         collection = self.db.get_collection(collection)
-        result = collection.update_one(data, new_data)
+        result = collection.update_one(filter, update)
 
         if result.acknowledged:
             if result.modified_count > 0:
-                return DataNotUpdatedException("No data to update")
-            return True
+                return True
+            return DataNotFoundException("No data to update")
         else:
             raise DataNotUpdatedException("Update failed")
 
-    def delete(self, data, collection=None):
+    def delete(self, filter, collection=None):
 
         if not collection:
             raise CollectionNotDefinedException("Collection is required")
-        if not data:
-            raise NoDataException("Data is required")
+        if not filter:
+            raise NoFilterException("Data is required")
 
         collection = self.db.get_collection(collection)
-        result = collection.delete_one(data)
+        result = collection.delete_one(filter)
 
         if result.acknowledged:
             if result.deleted_count > 0:
                 return True
-            raise DataNotDeletedException("No data to delete")
+            raise DataNotFoundException("No data to delete")
         else:
-            raise DataNotDeletedException("Data not deleted")
+            raise DataNotDeletedException("Deleted failed")
