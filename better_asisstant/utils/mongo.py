351003b283a625bf631efeaa35a2c15150981594
siha <zksi67@gmail.com>
2025-01-26 21:55:21 +0900

    UDATE error handing

--- better_asisstant/utils/mongo.py
+++ better_asisstant/utils/mongo.py
@@ -1,34 +1,86 @@
+import os
+
 from pymongo import MongoClient
-from better_asisstant.exceptions import NoDataException
+
+from better_asisstant.exceptions import (
+    CollectionNotDefinedException,
+    DataNotCreatedException,
+    DataNotDeletedException,
+    DataNotReadException,
+    DataNotUpdatedException,
+    NoDataException,
+)
+
 
 class mongo_clinet:
     def __init__(self):
-        self.client = MongoClient('mongodb://localhost:27017/')
-        self.db = self.client['better_assistant']
-        self.collection = self.db['user']
+        mongo_host = os.getenv("MONGO_HOST")
+        mongo_port = os.getenv("MONGO_PORT")
+        mongo_db = os.getenv("MONGO_DB")
 
-    def insert(self, data):
+        self.client = MongoClient(host=mongo_host, port=int(mongo_port))
+        self.db = self.client.get_database(mongo_db)
+
+    def create(self, data, collection=None):
+
+        if not collection:
+            raise CollectionNotDefinedException("Collection is required")
         if not data:
             raise NoDataException("Data is required")
-        
-        self.collection.insert_one(data)
+        collection = self.db.get_collection(collection)
+        result = collection.insert_one(data)
 
-    def find(self, data):
+        if result.acknowledged:
+            return result.inserted_id
+        else:
+            raise DataNotCreatedException("Data not created")
+
+
+    def read(self, data, collection=None):
+
+        if not collection:
+            raise CollectionNotDefinedException("Collection is required")
         if not data:
             raise NoDataException("Data is required")
-        
-        return self.collection.find_one(data)
+        collection = self.db.get_collection(collection)
+        result = collection.find_one(data)
 
-    def update(self, data, new_data):
+        if result:
+            return result
+        else:
+            raise DataNotReadException("Data not found")
+
+    def update(self, data, new_data, collection=None):
+
+        if not collection:
+            raise CollectionNotDefinedException("Collection is required")
         if not data:
             raise NoDataException("Data is required")
         if not new_data:
             raise NoDataException("New data is required")
-        
-        self.collection.update_one(data, new_data)
+        collection = self.db.get_collection(collection)
+        result = collection.update_one(data, new_data)
 
-    def delete(self, data):
+        if result.acknowledged:
+            if result.modified_count > 0:
+                return DataNotUpdatedException("No data to update")
+            return True
+        else:
+            raise DataNotUpdatedException("Update failed")
+
+    def delete(self, data, collection=None):
+
+        if not collection:
+            raise CollectionNotDefinedException("Collection is required")
         if not data:
             raise NoDataException("Data is required")
-        
-        self.collection.delete_one(data)
+
+        collection = self.db.get_collection(collection)
+        result = collection.delete_one(data)
+
+        if result.acknowledged:
+            if result.deleted_count > 0:
+                return True
+            raise DataNotDeletedException("No data to delete")
+        else:
+            raise DataNotDeletedException("Data not deleted")
