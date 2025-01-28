02ada3b01063de5521fb1982dd74deecd2cf6566
siha <zksi67@gmail.com>
2025-01-28 20:34:56 +0900

    Refactor HistoryManager to use async/await for database operations

--- better_assistant/managers/history.py
+++ better_assistant/managers/history.py
@@ -1,4 +1,7 @@
-from better_assistant.models import Dialog, MongoFilter
+from typing import List
+
+from better_assistant.models import Dialog, MongoFilter, MongoUpdate
+from better_assistant.models.models import Msg
 from better_assistant.utils import MongoClientWrapper
 
 
@@ -6,40 +9,61 @@ class HistoryManager:
     def __init__(self):
         self.mongo = MongoClientWrapper()
 
-    def get_dialogs(self):
+    async def get_dialogs(self):
         filter_obj = (
             MongoFilter()
-            .exists("dialog_title")
-            .exists("update_at")
-            .build()
+            .fields(["dialog_title", "updated_at"])
+            .build_with_projection()
         )
 
-        return list(self.mongo.find(filter_obj, 'dialogs'))
+        return await self.mongo.find(filter_obj, 'dialogs')
 
-    def get_dialog(self, dialog_id) -> Dialog:
+    async def get_dialog(self, dialog_id) -> Dialog:
+        filter_obj = (
+            MongoFilter()
+            .equals("dialog_id", dialog_id)
+            .fields(["dialog_title", "dialog_content", "updated_at"])
+            .build_with_projection()
+        )
+        result = self.mongo.find(filter_obj, 'dialogs')[0]
+        return await Dialog.from_dict(result)
+
+    async def post_dialog(self, dialog: Dialog):
+        return await self.mongo.insert(dialog, 'dialogs')
+
+    async def update_dialog(self, dialog_id, dialog):
         filter_obj = (
             MongoFilter()
             .equals("dialog_id", dialog_id)
             .build()
         )
-        result = (list(self.mongo.find(filter_obj, 'dialogs')))[0]
-        return Dialog.from_dictresult(result)
+        update_obj = (
+            MongoUpdate()
+            .set("dialog_title", dialog.dialog_title)
+            .set("dialog_content", dialog.dialog_content)
+            .set_updated_at()
+            .build()
+        )
+        return await self.mongo.update(filter_obj, update_obj, 'dialogs')
 
-    def post_dialog(self, dialog: Dialog):
-        return self.mongo.insert(dialog, 'dialogs')
-
-    def update_dialog(self, dialog_id, dialog):
+    async def push_new_messages(self, dialog_id, new_messages: List[Msg]):
         filter_obj = (
             MongoFilter()
             .equals("dialog_id", dialog_id)
             .build()
         )
-        return self.mongo.update(filter_obj, dialog, 'dialogs')
+        update_obj = (
+            MongoUpdate()
+            .push("dialog_content", new_messages)
+            .set_updated_at()
+            .build()
+        )
+        return await self.mongo.update(filter_obj, update_obj, 'dialogs')
 
-    def delete_dialog(self, dialog_id):
+    async def delete_dialog(self, dialog_id):
         filter_obj = (
             MongoFilter()
             .equals("dialog_id", dialog_id)
             .build()
         )
-        return self.mongo.delete(filter_obj, 'dialogs')
+        return await self.mongo.delete(filter_obj, 'dialogs')
