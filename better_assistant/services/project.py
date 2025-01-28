d8876380afe451a91e01865a4222304c5a425976
siha <zksi67@gmail.com>
2025-01-28 20:35:21 +0900

    UPDATE error conjection and project services

---
+++ better_assistant/services/project.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+from typing import Dict, List
+
+from bson import ObjectId
+
+from better_assistant.models import MongoFilter, MongoUpdate, Project
+from better_assistant.utils import MongoClientWrapper
+
+
+class ProjectService:
+    def __init__(self):
+        self.mongo_client = MongoClientWrapper()
+
+    async def get_projects(self) -> List[Dict[str, any]]:
+        filter_obj = (
+            MongoFilter()
+            .exists("project_title")
+            .fields(["project_title", "updated_at", "_id"])
+            .build_with_projection()
+            )
+        result: List[Dict[str, any]] = await self.mongo_client.find(filter_obj, collection_name="projects")
+
+        for data in result:
+            for key, value in data.items():
+                if isinstance(value, datetime):
+                    data[key] = value.strftime("%Y-%m-%d %H:%M:%S")
+                if isinstance(value, ObjectId):
+                    data[key] = value.__str__()
+        return result
+
+    async def get_project(self, project_id: str) -> dict:
+        filter_obj = (
+            MongoFilter()
+            .equals("_id", ObjectId(project_id))
+            .fields(["project_title", "updated_at"])
+            .build_with_projection()
+            )
+        result = await self.mongo_client.find(filter_obj, collection_name="projects")
+        return Project.from_dict(data=result[0]).to_dict()
+
+    async def create_project(self, project: Project) -> ObjectId:
+        return await self.mongo_client.insert(project, "projects")
+
+    async def update_project(self, project_id: str, project: Project) -> bool:
+        filter_obj = (
+            MongoFilter()
+            .equals("_id", ObjectId(project_id))
+            .build()
+            )
+        update_obj = (
+            MongoUpdate()
+            .set("project_title", project.project_title)
+            .set_updated_at()
+            .build()
+        )
+        return await self.mongo_client.update(filter_obj, update_obj, collection_name="projects")
+
+    async def delete_project(self, project_id: str) -> bool:
+        filter_obj = (
+            MongoFilter()
+            .equals("_id", ObjectId(project_id))
+            .build()
+            )
+        return await self.mongo_client.delete(filter_obj, collection_name="projects")
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import os
+from contextlib import asynccontextmanager
 
+from bson import ObjectId
 from fastapi import FastAPI, HTTPException, Query, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, StreamingResponse
+from loguru import logger
 
-app = FastAPI()
+from better_assistant.exceptions import (
+    CollectionNotDefinedException,
+    DataNotCreatedException,
+    DataNotFoundException,
+    NoDataException,
+    NoFilterException,
+)
+from better_assistant.models import Project
+from better_assistant.services import ProjectService
+
+project_service: ProjectService = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    서버 시작 시 초기화 작업을 위한 함수
+    """
+    global project_service
+    project_service = ProjectService()
+    logger.add("app.log", rotation="500 MB", format="{time} {level} {message}", level="DEBUG", enqueue=True)
+
+    # await project_service.mongo_client.__create_index__()
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [os.getenv("ALLOW_ORIGIN")]
 
@@ -34,10 +62,21 @@ async def fetch_projects():
     Returns:
         Response: 프로젝트 목록
     """
-    pass
+    try:
+        result = await project_service.get_projects()
+        return JSONResponse({"projects": result})
+    except CollectionNotDefinedException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except NoFilterException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except DataNotFoundException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=404, content="No data found.")
 
 @app.get("/project")
-async def fetch_project(project_id: str):
+async def fetch_project(projectId: str):
     """
     프로젝트 detail 호출을 위한 API
 
@@ -47,41 +86,82 @@ async def fetch_project(project_id: str):
     Returns:
         Response: {project_id}에 해당하는 프로젝트 정보
     """
-    # TODO: Implement project creation logic
-    return JSONResponse(content={"project_id": project_id})
+    try:
+        result: dict = await project_service.get_project(projectId)
+        return JSONResponse({"project_detail": result})
+    except CollectionNotDefinedException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except NoFilterException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except DataNotFoundException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=404, content=f"No data found in requested project id: {projectId}.")
+
 
 @app.post("/project")
-async def create_project():
+async def create_project(new_project: Project):
     """
     프로젝트 생성 API
 
     Returns:
         Response: 생성된 프로젝트 정보
     """
-    # TODO: Implement project creation logic
-    return JSONResponse(content={"project_id": "new_project_id"})
+    try:
+        result: ObjectId = await project_service.create_project(new_project)
+        return JSONResponse({"project_id": str(result)})
+    except CollectionNotDefinedException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except NoFilterException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except DataNotCreatedException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
 
 @app.put("/project")
-async def update_project(project_id: str):
+async def update_project(projectId: str, updated_project: Project):
     """
     프로젝트 수정 API
 
     Returns:
         Response: 수정된 프로젝트 정보
     """
-    # TODO: Implement project update logic
-    return JSONResponse(content={"project_id": "updated_project_id"})
+    try:
+        result: bool = await project_service.update_project(projectId, updated_project)
+        return JSONResponse({"sucess": result})
+    except CollectionNotDefinedException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except NoFilterException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=500, content="Contect to administator.")
+    except NoDataException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=422, content="No data to update.")
+    except DataNotFoundException as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return Response(status_code=404, content="No data found to update.")
 
 @app.delete("/project")
-async def delete_project(project_id: str):
+async def delete_project(projectId: str):
     """
     프로젝트 삭제 API
 
     Returns:
         Response: 삭제된 프로젝트 정보
     """
-    # TODO: Implement project deletion logic
-    return JSONResponse(content={"project_id": "deleted_project_id"})
+    try:
+        result: bool = await project_service.delete_project(projectId)
+        return JSONResponse(content={"sucess": result})
+    except CollectionNotDefinedException:
+        return Response(status_code=500, content="Contect to administator.")
+    except NoFilterException:
+        return Response(status_code=500, content="Contect to administator.")
+    except DataNotFoundException:
+        return Response(status_code=404, content="No data found to delete.")
 
 @app.post("/prompt")
 async def create_prompt(project_id: str):
