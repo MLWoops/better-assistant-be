2d27cb0402efab59af003153e556c5063a36e809
siha <zksi67@gmail.com>
2025-01-28 20:33:37 +0900

    UPDATE MongoDB connection and index creation logic

--- better_assistant/utils/mongo.py
+++ better_assistant/utils/mongo.py
@@ -1,95 +1,108 @@
 import os
+from typing import Dict, List
 
-from bson import objectid
-from pymongo import MongoClient
+from bson import ObjectId
+from pymongo import AsyncMongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+from pymongo.server_api import ServerApi
 
 from better_assistant.exceptions import (
     CollectionNotDefinedException,
     DataNotCreatedException,
-    DataNotDeletedException,
     DataNotFoundException,
-    DataNotUpdatedException,
+    NoDataException,
     NoFilterException,
 )
-from better_assistant.models import MongoDocument, MongoFilter
 
 
 class MongoClientWrapper:
     def __init__(self):
-        mongo_host = os.getenv("MONGO_HOST")
-        mongo_port = os.getenv("MONGO_PORT")
-        mongo_db = os.getenv("MONGO_DB")
+        mongo_host = os.getenv("MONGO_MLWOOPS_HOST")
+        mongo_user = os.getenv("MONGO_USER")
+        mongo_pass = os.getenv("MONGO_PASS")
+        mongo_db = os.getenv("MONGO_BA_DB")
 
-        self.client = MongoClient(host=mongo_host, port=int(mongo_port))
+        uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@{mongo_host}/?retryWrites=true&w=majority&appName=MLWoops"
+
+        print(f"Connecting to MongoDB: {mongo_host}")
+
+        self.client = AsyncMongoClient(
+            uri,
+            server_api=ServerApi('1'),
+            )
         self.db = self.client.get_database(mongo_db)
 
-    def __create_index__(self):
-        self.db.get_collection("projects").create_index("project_title", unique=True)
-        self.db.get_collection("prompts").create_index("prompt_version", unique=True)
-        self.db.get_collection("dialogs").create_index("dialog_title", unique=True)
+    async def __create_index__(self):
+        print("Creating indexes...")
+        try:
+            await self.db.get_collection("projects").create_index("project_title", unique=True, background=True)
+            await self.db.get_collection("prompts").create_index("prompt_version", unique=True, background=True)
+            await self.db.get_collection("dialogs").create_index("dialog_title", unique=True, background=True)
+        except ServerSelectionTimeoutError:
+            print("Failed to create indexes. Collection might not exist yet.")
 
-    def insert(self, document: MongoDocument, collection: str=None) -> objectid:
+    async def insert(self, document: "MongoDocument", collection_name: str=None) -> ObjectId: # noqa
 
-        if not collection:
+        if not collection_name:
             raise CollectionNotDefinedException("Collection is required")
         if not document:
             raise NoFilterException("Data is required")
-        collection = self.db.get_collection(collection)
+        collection = self.db.get_collection(collection_name)
 
         document = document.to_dict()
-        result = collection.insert_one(document)
+
+        result = await collection.insert_one(document)
 
         if result.acknowledged:
             return result.inserted_id
-        else:
-            raise DataNotCreatedException("Data not created")
+
+        raise DataNotCreatedException("Data not created")
 
 
-    def find(self, filter: MongoFilter, collection: str=None) -> MongoDocument:
+    async def find(self, filter: Dict[str, Dict[str, any]], collection_name: str=None) -> List:
 
-        if not collection:
+        if not collection_name:
             raise CollectionNotDefinedException("Collection is required")
         if not filter:
             raise NoFilterException("Data is required")
-        collection = self.db.get_collection(collection)
-        result = collection.find(filter)
+        collection = self.db.get_collection(collection_name)
 
-        if result:
+        cursor = collection.find(filter=filter["filter"], projection=filter["projection"])
+        result = await cursor.to_list(length=None)
+
+        if result and len(result) > 0:
             return result
-        else:
-            raise DataNotFoundException("Data not found")
+        raise DataNotFoundException(f"No data found for filter: {filter} in collection: {collection_name}")
 
-    def update(self, filter: MongoFilter, update, collection: str=None) -> bool:
 
-        if not collection:
+    async def update(self, filter: Dict[str, any], update: Dict[str, any], collection_name: str=None) -> bool:
+
+        if not collection_name:
             raise CollectionNotDefinedException("Collection is required")
         if not filter:
             raise NoFilterException("Data is required")
         if not update:
-            raise NoFilterException("New data is required")
-        collection = self.db.get_collection(collection)
-        result = collection.update_one(filter, update)
+            raise NoDataException("New data is required")
+        collection = self.db.get_collection(collection_name)
+        result = await collection.update_one(filter, update)
 
         if result.acknowledged:
             if result.modified_count > 0:
                 return True
-            raise DataNotFoundException("No data to update")
-        else:
-            raise DataNotUpdatedException("Update failed")
+        raise DataNotFoundException(f"No data found for filter: {filter} in collection: {collection_name}")
 
-    def delete(self, filter: MongoFilter, collection: str=None) -> bool:
 
-        if not collection:
+    async def delete(self, filter: Dict[str, any], collection_name: str=None) -> bool:
+
+        if not collection_name:
             raise CollectionNotDefinedException("Collection is required")
         if not filter:
             raise NoFilterException("Data is required")
 
-        collection = self.db.get_collection(collection)
-        result = collection.delete_one(filter)
+        collection = self.db.get_collection(collection_name)
+        result = await collection.delete_one(filter)
 
         if result.acknowledged:
             if result.deleted_count > 0:
                 return True
-            raise DataNotFoundException("No data to delete")
-        else:
-            raise DataNotDeletedException("Deleted failed")
+        raise DataNotFoundException(f"No data found for filter: {filter} in collection: {collection_name}")
